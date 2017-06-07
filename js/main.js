03a98c058db2b594f9ce550a5be134b2606b9ba3
Bobby Erickson <roericks@cisco.com>
2017-06-07 17:27:59 -0400

    Started incorporating angular into the app (including ngRoute)

--- js/main.js
+++ js/main.js
@@ -1,23 +1,8 @@
-(function(){
+var app = angular.module('testApp', ['ngRoute']);
 
-  $('.navigation a').click(function(){
-    var nav_link = $(this).text();
-    // TODO: turn this if/elseif statement into a switch statement
-    if(nav_link === 'Home'){
-      $('#home').show();
-      $('#portfolio').hide();
-      $('#contact').hide();
-    }
-    else if(nav_link === 'Portfolio'){
-      $('#portfolio').show();
-      $('#home').hide();
-      $('#contact').hide();
-    }
-    else if (nav_link === 'Contact'){
-      $('#contact').show();
-      $('#home').hide();
-      $('#portfolio').hide();
-    };
+app.config(['$routeProvider', function($routeProvider){
+
+  $routeProvider.when('/work', {
+    templateUrl: 'views/work.html'
   });
-
-})();
+}]);
