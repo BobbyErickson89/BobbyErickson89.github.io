0620d3b2420c04dab4374509cc1fca389fafbb52
Bobby Erickson <ericksonr07@gmail.com>
2017-12-30 14:56:26 -0500

    saving header redo

--- js/main.js
+++ js/main.js
@@ -1,8 +1,1 @@
-var app = angular.module('testApp', ['ngRoute']);
-
-app.config(['$routeProvider', function($routeProvider){
-
-  $routeProvider.when('/work', {
-    templateUrl: 'views/work.html'
-  });
-}]);
+console.log('test');
