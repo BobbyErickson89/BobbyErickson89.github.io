e16aada97432f76288c794d48b6e1852fbef15b8
Bobby Erickson <ericksonr07@gmail.com>
2018-01-15 15:17:45 -0500

    Navigation sticky added
    Added JS for sticking the navbar to the top of the page when we scroll
    past it

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-console.log('test');
+let viewportHeight = window.innerHeight;
+let sticky = false;
+let nav_bar = document.getElementsByClassName("nav-bar");
+nav_bar = nav_bar[0];
+
+//whenever our window is resized, we are re-setting our viewport variable.
+let windowResize = () => {
+    viewportHeight = window.innerHeight;
+};
+
+//This function is adding a sticky class to our navbar, so that it will
+// be fixed to the top of the page when we scroll past the intro section.
+let setStickyNavbar = () => {
+    if(window.scrollY > viewportHeight && sticky === false){
+        nav_bar.classList.add('sticky');
+        sticky = true;
+    } else if(sticky === true && window.scrollY < viewportHeight){
+        nav_bar.classList.remove('sticky');
+        sticky = false;
+    }
+};
+
+window.onresize = windowResize;
+window.onscroll = setStickyNavbar;
