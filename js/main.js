034314c251b5aff20d0550e9c77c288dd3c37ca9
Bobby Erickson <ericksonr07@gmail.com>
2018-01-15 16:15:04 -0500

    Smooth scroll and navbar adjustments
    We now have a smooth scroll to the ‘About Me’ section.  I also
    corrected a minor bug in the navbar sticky.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,42 @@
-let viewportHeight = window.innerHeight;
-let sticky = false;
-let nav_bar = document.getElementsByClassName("nav-bar");
-nav_bar = nav_bar[0];
+//////// VARIABLES
+let viewport_height = window.innerHeight,
+    sticky = false,
+    nav_bar = document.querySelector('.nav-bar'),
+    nav_bar_sibling = nav_bar.nextElementSibling,
+    learn_more_btn = document.querySelector('.learn-more-btn');
 
+
+/////// FUNCTIONS
 //whenever our window is resized, we are re-setting our viewport variable.
 let windowResize = () => {
-    viewportHeight = window.innerHeight;
+    viewport_height = window.innerHeight;
 };
 
 //This function is adding a sticky class to our navbar, so that it will
 // be fixed to the top of the page when we scroll past the intro section.
 let setStickyNavbar = () => {
-    if(window.scrollY > viewportHeight && sticky === false){
+    if(window.scrollY > viewport_height && sticky === false){
+        let height = nav_bar.clientHeight;
         nav_bar.classList.add('sticky');
+        nav_bar_sibling.style.marginTop = `${height}px`; //this is making up for navbars height becoming a fixed element.
         sticky = true;
-    } else if(sticky === true && window.scrollY < viewportHeight){
+    } else if(sticky === true && window.scrollY < viewport_height){
         nav_bar.classList.remove('sticky');
+        nav_bar_sibling.style.marginTop = 'auto'; //removing the extra margin we gave the sibling when making navbar sticky.
         sticky = false;
     }
 };
 
+let scrollToAboutMe = () => {
+    window.scroll({
+        top: viewport_height,
+        left: 0,
+        behavior: 'smooth',
+    });
+};
+
+
+//////// EVENTS
 window.onresize = windowResize;
 window.onscroll = setStickyNavbar;
+learn_more_btn.onclick = scrollToAboutMe;
